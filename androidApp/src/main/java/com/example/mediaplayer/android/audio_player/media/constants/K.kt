554e1400988a7095180985e663805d3e40ab05f5
sahushubham <sahushubham2208@gmail.com>
2023-04-24 18:39:36 +0530

    Add function MediaPlayerService and MediaPlayerNotificationManager

--- androidApp/src/main/java/com/example/mediaplayer/android/audio_player/media/constants/K.kt
+++ androidApp/src/main/java/com/example/mediaplayer/android/audio_player/media/constants/K.kt
@@ -1,10 +1,12 @@
 package com.example.mediaplayer.android.audio_player.media.constants
 
 object K {
-    const val PLAYBACK_NOTIFICATION_CHANNEL_ID = "jbhvhdbvsdhbh"
-    const val PLAYBACK_NOTIFICATION_ID = 128251658161
-    const val MEDIA_ROOT_ID = "sdfhaihfadbfasdk"
+    const val PLAYBACK_NOTIFICATION_CHANNEL_ID = "dhfsjhdsjdhdhhdhdh"
+    const val PLAYBACK_NOTIFICATION_ID = 1267
+    const val MEDIA_ROOT_ID = "hhdsjhkfsjdjhdhhd"
     const val START_MEDIA_PLAY_ACTION = "START_MEDIA_PLAY_ACTION"
-    const val REFRESH_MEDIA_PLAY_ACTION = "REFRESH_MEDIA_PLAY_ACTION"
-    const val PLAYBACK_UPDATE_INTERNAL = 1000L
+    const val REFRESH_MEDIA_PLAY_ACTION = "REFRESH_MEDIA_PLAY_ACTION "
+    const val PLAYBACK_UPDATE_INTERVAL = 1000L
+
+
 }
