28ea8ab28a43913e6890e72e1922ae9b580c9c3f
sahushubham <sahushubham2208@gmail.com>
2023-05-01 11:55:53 +0530

    Add Media Player with Audio mp3 store in raw in App

--- androidApp/src/main/java/com/example/mediaplayer/android/view/bottom_navigation_screen/Audio.kt
+++ androidApp/src/main/java/com/example/mediaplayer/android/view/bottom_navigation_screen/Audio.kt
@@ -1,23 +1,43 @@
 package com.example.mediaplayer.android.view.bottom_navigation_screen
 
+import android.media.MediaPlayer
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import com.example.mediaplayer.android.R
 
 @Composable
 fun Audio() {
-    Box(
+    val context = LocalContext.current
+    var mediaPlayer = MediaPlayer.create(context, R.raw.music)
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.White),
-        contentAlignment = Alignment.Center
+        horizontalAlignment =Alignment.CenterHorizontally
     ) {
+        Button(onClick = {
+            mediaPlayer.start()
+        })
+        {
+            Text(text = "Play")
+        }
+        Button(onClick = {
+            mediaPlayer.stop()
+            mediaPlayer.prepare()
+        })
+        {
+            Text(text = "stop")
+        }
 
 
     }
